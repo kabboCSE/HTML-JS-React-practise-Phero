4631d6181a866f54458bb597b1e5be7c106aed2a
kabboCSE <shahriar15-10494@diu.edu.bd>
2025-09-23 00:50:16 +0600

    new

---
+++ react-world-on-the-go2/src/components/Country/Country.jsx
@@ -0,0 +1,27 @@
+import React, { useState } from "react";
+import "./Country.css";
+
+const Country = ({ country }) => {
+  const [visited, setVisited] = useState(false);
+
+  const handleVisited = () => {
+    setVisited(true);
+  };
+  return (
+    <div className="country">
+      {/* Optional Chaining */}
+      <img src={country?.flags?.flags?.png} alt={country.flags.flags.alt} />
+      <h3>Name: {country.name.common}</h3>
+      <p>Population: {country.population.population} </p>
+      <p> Area: {country.area.area} </p>
+      <p style={{ color: "skyblue" }}>
+        {country.area.area > 300000 ? "Big Country" : "Small Country"}
+      </p>
+      <button onClick={handleVisited}>
+        {visited ? "Visited" : "Not Visited"}
+      </button>
+    </div>
+  );
+};
+
+export default Country;
