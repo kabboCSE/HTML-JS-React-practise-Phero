b8a71e30ec49c5401e18867ec7bcc0a5c1e294ae
kabboCSE <shahriar15-10494@diu.edu.bd>
2025-09-25 01:48:39 +0600

    update

--- react-world-on-the-go2/src/components/Countries/Countries.jsx
+++ react-world-on-the-go2/src/components/Countries/Countries.jsx
@@ -4,9 +4,16 @@ import "./Countries.css";
 
 const Countries = ({ countriesPromise }) => {
   const [visitedCountries, setvisitedCountries] = useState([]);
+  const [visitedFlags, setVisitedFlags] = useState([]);
 
   const handleVisitedCountries = (country) => {
-    console.log(country.name.common);
+    // console.log(country.name.common);
+    const newVisitedCountries = [...visitedCountries, country];
+    setvisitedCountries(newVisitedCountries);
+  };
+
+  const handleVisitedFlag = () => {
+    console.log("Flag need to be added");
   };
   const countriesData = use(countriesPromise);
   const countries = countriesData.countries;
@@ -15,12 +22,18 @@ const Countries = ({ countriesPromise }) => {
     <div>
       <h1>In the Countries : {countries.length}</h1>
       <h2>Total Visited Country : {visitedCountries.length} </h2>
+      <ol>
+        {visitedCountries.map((country) => (
+          <li key={country.cca3.cca3}>{country.name.common}</li>
+        ))}
+      </ol>
       <div className="countries">
         {countries.map((country) => (
           <Country
             key={country.cca3.cca3}
             handleVisitedCountries={handleVisitedCountries}
             country={country}
+            handleVisitedFlag={handleVisitedFlag}
           ></Country>
         ))}
       </div>
--- react-world-on-the-go2/src/components/Country/Country.jsx
+++ react-world-on-the-go2/src/components/Country/Country.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import "./Country.css";
 
-const Country = ({ country, handleVisitedCountries }) => {
+const Country = ({ country, handleVisitedCountries, handleVisitedFlag }) => {
   const [visited, setVisited] = useState(false);
 
   const handleVisited = () => {
@@ -24,6 +24,7 @@ const Country = ({ country, handleVisitedCountries }) => {
       <button onClick={handleVisited}>
         {visited ? "Visited" : "Not Visited"}
       </button>
+      <button onClick={handleVisitedFlag}>Add Visited Flag</button>
     </div>
   );
 };
